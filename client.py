a62866a868718b2597a0adbcf7742a727a43e10c
Augusto Ribeiro <rguto11@gmail.com>
2026-03-05 20:19:23 -0300

    Implementando conexão e mensagens

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 import socket
 CONFIG = {
-    "HOST": "localhost", # Localhost para testes internos
+    "HOST": "127.0.0.1", # Localhost para testes internos
     "PORTA": 5000         # Guichê de atendimento do leilão
 }
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 cliente.connect((CONFIG["HOST"], CONFIG["PORTA"]))
+
+dados_iniciais = cliente.recv(1024).decode() #meio que faz ele receber os dados e só pode receber até 1024 bytes de uma vez
+print(dados_iniciais)
--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 import socket
+import threading
+import time
 
 # Configurações do Servidor (host e porta do servidor)
 CONFIG = {
     "HOST": "127.0.0.1", # Localhost para testes internos
     "PORTA": 5000         # Guichê de atendimento do leilão
 }
+
+#o item do leilao atual que vai anunciar
+item_leilao = {
+    "item": "Quadro Monalisa",
+    "valor_atual": 900000,
+    "tempo": 60
+}
+
+lock = threading.Lock() # apenas uma memória mexe na memória compartilhada
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,5 +24,12 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((CONFIG["HOST"], CONFIG["PORTA"]))
 server.listen() 
-print("Servidor pronto e ouvindo na porta 5000...")
+
+print("Servidor pronto. \nItem: {0}\nLance inicial: R${1}",(item_leilao['item'],item_leilao["valor_atual"]))
+
 conn, addr = server.accept()
+horario = time.strftime("%H:%M")
+
+
+mensagem_inicial = f"Leilão aberto, são {horario} \n\n\nItem:{item_leilao['item']} \nValor: R${item_leilao['valor_atual']}"
+conn.send(mensagem_inicial.encode()) #envia para o cliente(codificar para bytes(pq o cabo de rede só entende bytes, bits etc))
